49f2c502aec44e2dd4d74f18cf8de0e79d43801f
Lolinonusos <linus.nordbakken@gmail.com>
2022-02-24 17:36:52 +0100

    Added some cpp classes

---
+++ Source/Comp3/Comp3GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Comp3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMP3_API AComp3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
