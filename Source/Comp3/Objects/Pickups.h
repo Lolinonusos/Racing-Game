2ede1a60e9828dde2e8874b380f74b218b7991ef
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2022-03-11 15:21:48 +0100

    Added classes
    For Powerups and pickups

---
+++ Source/Comp3/Objects/Pickups.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Powerups.h"
+#include "Pickups.generated.h"
+
+
+UCLASS()
+class COMP3_API APickups : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickups();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickups")
+		class UStaticMeshComponent* PickupStaticMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickups")
+		TArray<APowerups*> PickupContentList;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickups")
+		FVector PickupPosition;
+};
