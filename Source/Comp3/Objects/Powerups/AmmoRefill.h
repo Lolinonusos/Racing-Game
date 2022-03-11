f6fc25ab352c88497b52f6328da54f0b5fc52b39
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2022-03-11 15:48:06 +0100

    Imrpoved? Powerups

---
+++ Source/Comp3/Objects/Powerups/AmmoRefill.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Powerups.h"
+#include "AmmoRefill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMP3_API AAmmoRefill : public APowerups
+{
+	GENERATED_BODY()
+	
+public:
+	AAmmoRefill();
+};
