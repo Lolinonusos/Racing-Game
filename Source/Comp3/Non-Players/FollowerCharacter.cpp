e55296d22aeb345a081583274e0631f6a61f1873
Lolinonusos <linus.nordbakken@gmail.com>
2022-05-06 10:40:52 +0200

    Ai character added, time for school

--- Source/Comp3/Non-Players/FollowerCharacter.cpp
+++ Source/Comp3/Non-Players/FollowerCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Non-Players/FollowerCharacter.h"
+#include "FollowerCharacter.h"
 
 // Sets default values
 AFollowerCharacter::AFollowerCharacter()
